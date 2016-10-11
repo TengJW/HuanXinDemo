9ecb6afe53562e858b84b19010b337eb68f357e2
yuanchao <yuanchao@feicuiedu.com>
2016-10-11 11:40:22 +0800

    demo配置，初始提交

---
+++ apphx/src/main/java/com/feicuiedu/apphx/HxBaseApplication.java
@@ -0,0 +1,33 @@
+package com.feicuiedu.apphx;
+
+import android.app.Application;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
+/**
+ * 环信相关基础配置
+ * 作者：yuanchao on 2016/10/11 0011 11:22
+ * 邮箱：yuanchao@feicuiedu.com
+ */
+public class HxBaseApplication extends Application {
+
+    // 用git管理当前项目，上传到github
+
+    // 1. 去gitbub上新建一个..
+    // 2. git管理自己的项目, 远程连接到github, 再push上去
+
+    @Override public void onCreate() {
+        super.onCreate();
+        // 初始化环信sdk和easeui库
+        initEaseUI();
+    }
+
+    private void initEaseUI() {
+        EMOptions options = new EMOptions();
+        options.setAutoLogin(false); // 关闭自动登录
+        EMClient.getInstance().init(this,options);
+        // 关闭环信日志
+        EMClient.getInstance().setDebugMode(false);
+    }
+}
